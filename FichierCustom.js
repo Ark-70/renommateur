a2af847a319eaf55873757c5efc2e9d3b3f0123c
David Traparic <david.traparic@etu.univ-poitiers.fr>
2018-04-17 00:25:44 +0200

    class FichierCustom ajoutée, mais code reste très dirty

--- FichierCustom.js
+++ FichierCustom.js
@@ -1,21 +1,82 @@
 class FichierCustom {
   constructor(fileObj) {
     // variables partagées entre toutes les instances de la classes, qui ne changeront pas
-    static this.prefix = undefined;
-    static this.suffix = undefined;
-    static this.forbiddenWords = [];
+    // Pas sûr que le static variable soit pris en compte en ES6
+    // static prefix = undefined;
+    // static suffix = undefined;
+    // static forbiddenWords = [];
 
 
     this.originalName = fileObj.name;
     this.originalSize = fileObj.size;
     this.originalDate = fileObj.name;
+    this.newName;
+    this.betterSize;
+    this.betterDate;
   }
 
-  get newName(){
-    return generateNewName();
+  getBetterSize(){
+    mySize = this.originalSize;
+    if(mySize>=(Math.pow(1024,2))){
+      mySize = Number(mySize/(Math.pow(1024,2))).toFixed(2) + " Mo";
+    }else if(mySize>=(1024)){
+      mySize = Number(mySize/(1024)).toFixed(2) + " Ko";
+    }else{
+      mySize+=" octets"
+    }
+    this.betterSize = mySize;
+    return this.betterSize;
+  }
+
+  getWithoutExt(str){
+    let strNoExt = "";
+    strNoExt = str.split(".");
+    strNoExt.splice(strNoExt.length-1,1);
+    strNoExt = strNoExt.join(".");
+    return strNoExt;
   }
 
   generateNewName(){
-    return
+    this.newName = this.beautifyName(this.normalizeNameToArr(this.originalName));
+    return this.newName;
   }
+
+  normalizeNameToArr(fileName){
+    let arrName = fileName.split(/[\s,\-_]+/);
+    console.log(arrName);
+    for (var i = 0; i < arrName.length; i++) {
+      arrName[i] = arrName[i].toLowerCase();
+    }
+
+    return arrName;
+  }
+
+  beautifyName(arrName){
+    let tmpI = arrName.indexOf("cv");
+    if(tmpI!=-1) arrName.splice(tmpI, 1);
+    // Enlever des mots
+
+    // Mettre le premier mot (supposé NOM) en maj
+    arrName[0] = arrName[0].toUpperCase();
+    console.log(arrName);
+    if (arrName.length>=2) {
+      // arrName[1][0] = arrName[1][0].toUpperCase(); //marche pas ???
+      arrName[1] = arrName[1][0].toUpperCase()+arrName[1].slice(1, arrName[1].length);
+      console.log(arrName[1][0]);
+    }
+    let beautifiedName = arrName.join(' ');
+    return beautifiedName;
+  }
+
+  set prefix(prefix){
+    FichierCustom.prefix = prefix;
+  }
+  set suffix(suffix){
+    FichierCustom.suffix = suffix;
+  }
+  set forbiddenWords(forbiddenWords){
+    FichierCustom.forbiddenWords = forbiddenWords;
+  }
+
+
 }
--- script.js
+++ script.js
@@ -8,6 +8,7 @@ $(function(){
   // startWithTests(); // A ENLEVER NORMALEMENT NON ?
 
 let myNewReadyFiles = null;
+let mesFichiersCustom = [];
 
   document.querySelector('#tester').addEventListener("click", startWithTests);
   function startWithTests(){
@@ -72,111 +73,78 @@ let myNewReadyFiles = null;
   }
 
   function traiterFiles(files){
+
+    for (file of files) {
+      mesFichiersCustom.push(new FichierCustom(file));
+    }
+
     resetTable();
-    showFiles(files);
+    showFiles(mesFichiersCustom);
     let myNames = [];
     console.log("tab de base : ");
-    console.log(files);
-    for (file of files) {
-      myNames.push(normalizeIntoArrays(file.name));
+    console.log(mesFichiersCustom);
+    for (fichierCustom of mesFichiersCustom) {
+      fichierCustom.generateNewName();
     }
-    console.log("myNames : ");
-    console.log(myNames);
-    let beautifiedNames = beautifyNames(myNames);
-    console.log("beautifiedNames : ")
-    console.log(beautifiedNames);
 
-    //display new names
     $('#thnewname').show();
-
-
     $firstCellsOfEachRowAvailable = $('td:first-child');
 
+    let allNewNames = [];
+    for (var i = 0; i < mesFichiersCustom.length; i++) {
 
-    for (var i = 0; i < beautifiedNames.length; i++) {
-
-      let cellHTML = '<td class="tdnewname">'+getWithoutExt(beautifiedNames[i])+'</td>';
+      let cellHTML = '<td class="tdnewname">'+mesFichiersCustom[i].getWithoutExt(mesFichiersCustom[i].newName)+'</td>';
       $($firstCellsOfEachRowAvailable[i]).after(cellHTML);
+
+      allNewNames.push(mesFichiersCustom[i].newName);
     }
 
-    // //update files with new names
-    // if (files.length === beautifiedNames.length) {
-    //   for (var i = 0; i < files.length; i++) {
-    //     files[i].name = beautifiedNames[i];
-    //   }
-    //   myNewReadyFiles = JSON.stringify(files);
-    // }else{
-    //   console.log("ERROR : NOT SAME LENGTH BETWEEN NAMES & FILES");
-    // }
-
-
-
-    console.log(myNewReadyFiles);
-    console.log(files);
-    $('#datanames').val(JSON.stringify(beautifiedNames));
+    $('#datanames').val(JSON.stringify(allNewNames));
     $('#validate').show();
   }
 
 
-  function showFiles(files){
-    console.log(files);
-    for (file of files) {
+  function showFiles(fichiersCustom){
+    console.log(fichiersCustom);
+    for (fichierCustom of fichiersCustom) {
       // console.log(file);
       //enlever le dernier element d'un split(".")
-      mySize = file.size;
-      if(mySize>=(Math.pow(1024,2))){
-        mySize = Number(mySize/(Math.pow(1024,2))).toFixed(2) + " Mo";
-      }else if(mySize>=(1024)){
-        mySize = Number(mySize/(1024)).toFixed(2) + " Ko";
-      }else{
-        mySize+=" octets"
-      }
+
       // console.log(filenameWithoutExt);
-      $('tbody').append("<tr><td>"+getWithoutExt(file.name)+"</td><td>"+mySize+"</td><td>"+file.type+"</td><td>"+new Date(file.lastModified)+"</td></tr>");
+      $('tbody').append("<tr><td>"+fichierCustom.getWithoutExt(fichierCustom.originalName)+"</td><td>"+fichierCustom.originalSize+"</td><td>"+file.type+"</td><td>"+new Date(file.lastModified)+"</td></tr>");
     }
-  }
-
-  function normalizeIntoArrays(fileName){
-    console.log(fileName);
-    let arrName = fileName.split(/[\s,\-_]+/);
-    console.log(arrName);
-    for (var i = 0; i < arrName.length; i++) {
-      arrName[i] = arrName[i].toLowerCase();
-    }
-
-    return arrName;
   }
 
   function beautifyNames(arrNames){
-    for (let i = 0; i < arrNames.length; i++) {
-        let tmpI = arrNames[i].indexOf("cv");
-        if(tmpI!=-1) arrNames[i].splice(tmpI, 1);
-      // Enlever des mots
+    // for (let i = 0; i < arrNames.length; i++) {
+    //   let tmpI = arrNames[i].indexOf("cv");
+    //   if(tmpI!=-1) arrNames[i].splice(tmpI, 1);
+    //   // Enlever des mots
+    //
+    //   // Mettre le premier mot (supposé NOM) en maj
+    //   arrNames[i][0] = arrNames[i][0].toUpperCase();
+    //   console.log(arrNames[i]);
+    //   if (arrNames[i].length>=2) {
+    //     // arrNames[i][1][0] = arrNames[i][1][0].toUpperCase(); //marche pas ???
+    //     arrNames[i][1] = arrNames[i][1][0].toUpperCase()+arrNames[i][1].slice(1, arrNames[i][1].length);
+    //     console.log(arrNames[i][1][0]);
+    //   }
+    // }
 
-      // Mettre le premier mot (supposé NOM) en maj
-      arrNames[i][0] = arrNames[i][0].toUpperCase();
-      console.log(arrNames[i]);
-      if (arrNames[i].length>=2) {
-        // arrNames[i][1][0] = arrNames[i][1][0].toUpperCase(); //marche pas ???
-        arrNames[i][1] = arrNames[i][1][0].toUpperCase()+arrNames[i][1].slice(1, arrNames[i][1].length);
-        console.log(arrNames[i][1][0]);
-      }
-    }
-
-    let tmpBeautifiedArrNames = [];
-    console.log("aha");
-    for (let i = 0; i < arrNames.length; i++) {
-      tmpBeautifiedArrNames[i] = arrNames[i].join(' ');
-    }
-    return tmpBeautifiedArrNames;
+    // let tmpBeautifiedArrNames = [];
+    // console.log("aha");
+    // for (let i = 0; i < arrNames.length; i++) {
+    //   tmpBeautifiedArrNames[i] = arrNames[i].join(' ');
+    // }
+    // return tmpBeautifiedArrNames;
   }
 
 
-  function getWithoutExt(str){
-    strNoExt = str.split(".");
-    strNoExt.splice(strNoExt.length-1,1);
-    strNoExt = strNoExt.join(".");
-    return strNoExt
-  }
+  // function getWithoutExt(str){
+  //   strNoExt = str.split(".");
+  //   strNoExt.splice(strNoExt.length-1,1);
+  //   strNoExt = strNoExt.join(".");
+  //   return strNoExt
+  // }
 
 });
